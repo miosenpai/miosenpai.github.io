b8547a459de27576b2a10c2fa96f73a7d8e665b6
mio <knightopress@gmail.com>
2021-08-26 22:28:57 -0400

    Add Cirrus.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,18 @@
-import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
+import "cirrus-ui";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 export default MyApp;
